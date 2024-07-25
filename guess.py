d1d603a36fa77ebdf5c6aec947af3c0b0fd0f789
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-25 16:59:57 +0500

    I have changed code guesses

--- guess.py
+++ guess.py
@@ -3,6 +3,8 @@ n = random.randint(1,10)
 guess= int(input("1 dan 10 gacha son kiriting:"))
 guesses = 0
 while True:
+    guess=int(input("1 dan 10 gacha son kiriting: "))
+    guesses += 1
     if n == guess:
         print("Topdingiz")
         break   
