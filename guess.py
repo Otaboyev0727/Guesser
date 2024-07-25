384c5de03dc6463766a9ab21208221cf225bdb31
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-25 16:29:51 +0500

    syntext

--- guess.py
+++ guess.py
@@ -3,7 +3,7 @@ n = random.randint(1,10)
 guess= int(input("1 dan 10 gacha son kiriting:"))
 guesses = 0
 while True:
-    if n = =guess:
+    if n == guess:
         print("Topdingiz")
         break   
     if guesses >= 3:
