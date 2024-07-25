413294a7ddf264206cd400ba625a197ebd17f812
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-25 16:20:00 +0500

    limit number of guesses

--- guess.py
+++ guess.py
@@ -1,8 +1,11 @@
 import random
 n = random.randint(1,10)
 guess= int(input("1 dan 10 gacha son kiriting:"))
+guesses = 0
 while True:
-    if n==guess:
-       print("Topdingiz")
-       break   
-
+    if n = =guess:
+        print("Topdingiz")
+        break   
+    if guesses >= 3:
+        print("Yutqadizngiz")
+        break
