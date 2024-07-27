f4db1fe769b6ab4f8f32b642149b43c3dbd76686
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-27 18:16:29 +0500

    type hehre

--- guess.py
+++ guess.py
@@ -3,9 +3,9 @@ n = random.randint(1,10)
 guess= int(input("1 dan 10 gacha son kiriting:"))
 guesses = 0
 while True:
-    guess=int(input("1 dan 10 gacha son kiriting: "))
+    guess=  int(input("1 dan 10 gacha son kiriting: "))
     guesses += 1
-    if n == guess:
+    if n ==    guess:
         print("Topdingiz")
         break   
     if guesses >= 3:
