2e11bd009670f349b81aa7a611a0e1e434799781
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-30 21:46:20 +0500

    I know

--- new.py
+++ new.py
@@ -1,18 +1,19 @@
 import math
-import random 
+import random
+
 
 def sontop(x=10):
-    tasodifiy_son = random.randint(1,x)
+    tasodifiy_son = random.randint(1, x)
     print(f" Men 1 dan {x} gacha son o'yladim. Topa olasizmi?", end=" ")
     while True:
         taxmin = int(input(">>>"))
         if taxmin < tasodifiy_son:
             print("Kattaroq son ayting: ", end="")
-        elif taxmin>tasodifiy_son:
+        elif taxmin > tasodifiy_son:
             print("Kichikroq son ayting:", end="")
         else:
             print("YUTDINGIZ!!!")
             break
-sontop()            
-        
 
+
+sontop()
