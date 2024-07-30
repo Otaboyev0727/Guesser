e0556b79a0b42a640819d0463153cb5f5bfc875e
Otaboyev Egamberdi <egamberdi.o7otaboyev@gmail.com>
2024-07-30 21:39:33 +0500

    Write code find number

--- guess.py
+++ guess.py
@@ -1,13 +1,14 @@
 import random
-n = random.randint(1,10)
-guess= int(input("1 dan 10 gacha son kiriting:"))
+
+n = random.randint(1, 10)
+guess = int(input("1 dan 10 gacha son kiriting:"))
 guesses = 0
 while True:
-    guess=  int(input("1 dan 10 gacha son kiriting: "))
+    guess = int(input("1 dan 10 gacha son kiriting: "))
     guesses += 1
-    if n ==    guess:
+    if n == guess:
         print("Topdingiz")
-        break   
+        break
     if guesses >= 3:
         print("Yutqadingiz!!!")
         break
---
+++ new.py
@@ -0,0 +1,18 @@
+import math
+import random 
+
+def sontop(x=10):
+    tasodifiy_son = random.randint(1,x)
+    print(f" Men 1 dan {x} gacha son o'yladim. Topa olasizmi?", end=" ")
+    while True:
+        taxmin = int(input(">>>"))
+        if taxmin < tasodifiy_son:
+            print("Kattaroq son ayting: ", end="")
+        elif taxmin>tasodifiy_son:
+            print("Kichikroq son ayting:", end="")
+        else:
+            print("YUTDINGIZ!!!")
+            break
+sontop()            
+        
+
